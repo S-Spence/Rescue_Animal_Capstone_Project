efc89fa16519590a82f3f0a5484d16afe660c089
Sarah <sarah.spence1991@gmail.com>
2022-02-12 15:49:34 -0500

    Adding deployment configs

--- server.js
+++ server.js
@@ -7,13 +7,28 @@ require("dotenv").config({ path: "./config.env" });
 const app = express();
 // Define port
 const port = process.env.PORT || 5000;
+// Define path
+const path = require("path");
+
 // Use cors and express
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/animal"));
+app.use(require("./backend/routes/animal"));
 
 // Get the driver connection
-const db = require("./database/connection");
+const db = require("./backend/database/connection");
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__direname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
+
 
 app.listen(port, () => {
   // perform a database connection when server starts
